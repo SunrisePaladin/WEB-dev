7fac33d60492c1d743524b6dadb5bf310083f903
WEB-exam <artsolohome@gmail.com>
2023-02-17 18:45:57 +0300

    Format features

--- Жабаскрипт/lab1.js
+++ Жабаскрипт/lab1.js
@@ -4,11 +4,13 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
 }
 
+const myFunc = function () {};
+
 //Задача 1
 // let a  = prompt('Speed in km/h');
-// console.log(a + ` km/h - is ${a*1000/3600} m/s`);
+// console.log(`${a} km/h - is ${a*1000/3600} m/s`);
 // let b  = prompt('Speed in m/s');
-// console.log(b + ` m/s - is ${a/1000*3600} km/h`);
+// console.log('${b} + m/s - is ${b/1000*3600} km/h`);
 
 
 //задача 2
@@ -16,7 +18,6 @@ function getRandomInt(min, max) {
 // let b  = +prompt('сторона b');
 // let c  = +prompt('сторона c');
 // let p = (a+b+c)/2;
-
 // if (!(a+b>c && b+c>a && c+a>b)) console.log('треугольник не существует')
 // else {
 //     console.log('треугольник существует');
@@ -26,33 +27,36 @@ function getRandomInt(min, max) {
 // };
 
 
-//Задача 3
 // let ih = +prompt("длина", 11);
 // console.log(typeof(ih));
 // if (typeof(ih) === 'number') {
-// if(!(isNaN(ih))){ 
-// for (let i = 0; i < ih; i++){
-//     if (i%5 == 0 && i != 0){
-//         console.log(i, "fizz buzz");
-//         continue;
-//     }
-//     if (i%2 == 0){
-//     console.log(i, "buzz");
-//     }
-//     else console.log(i, "fizz");
-// };
+// if(!isNaN(ih)){ 
+//         for (let i = 0; i < ih; i++){
+//         if (i%5 == 0 && i != 0){
+//             console.log(i, "fizz buzz");
+//             continue;
+//         }
+//         if (i%2 == 0){
+//             console.log(i, "buzz");
+//         }
+//         else {
+//             console.log(i, "fizz");
+//         }
+//     };
 // }
-
 
 //Задача 4
 // let pine = '>';
 // for (let i = 1; i <=12; i++){
 //     pine = pine + "\n";
-//     if (i%2!=0)
-//         for (let j = 0; j < i; j++) {pine = pine + "*";}
-    
+//     if (i%2!=0) 
+//         for (let j = 0; j < i; j++) {
+//             pine = pine + "*";
+//         }
 //     else 
-//     for (let j = 0; j < i; j++) {pine = pine + "#";}
+//         for (let j = 0; j < i; j++) {
+//             pine = pine + "#";
+//         }
 // }
 // pine += "\n||";
 // console.log(pine);
@@ -72,14 +76,21 @@ function getRandomInt(min, max) {
 //     guess = prompt('Ваше число: ');
 // }
 
-//Задача 6
+// Задача 6
 // let d  = +prompt('Число');
 // let n1  = +prompt('Делитель 1');
 // let n2  = +prompt('Делитель 2');
 // let res;
-// (d%n1 == 0)? (d%n2 == 0) ? res=true : res = false : res=false;
+// if (d%n1 == 0){
+//     if (d%n2 == 0){
+//         res = true;
+//     }
+//     else res = false;
+// }
+// else res = false;
+// // (d%n1 == 0)? (d%n2 == 0) ? res=true : res = false : res=false;
 // console.log('n = ', d, ', x = ', n1, ', y = ', n2, ' => ', res);
 
 //Задача 7
-let mn = Math.abs(+prompt('Введите месяц'));
+let mn = +prompt('Введите месяц');
 console.log('месяц', mn, '=>', `${Math.ceil(mn/3)}`, 'квартал')
