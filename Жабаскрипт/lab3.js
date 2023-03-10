48824a22492ecc32723ee0d5a5cd42cad86f3c65
WEB-exam <artsolohome@gmail.com>
2023-03-10 18:08:43 +0300

    certain progress

--- Жабаскрипт/lab3.js
+++ Жабаскрипт/lab3.js
@@ -10,7 +10,7 @@ const body = document.body;
 const div = document.getElementById("div1");
 const form = document.getElementById("form1");
 const button = document.getElementById("btn1");
-const input1 = document.querySelector("input");
+const input = document.querySelector("input");
 
 // button.textContent = `<script defer src="кража_данных.js"></script>`;
 // button.innerHTML = `<script defer src="кража_данных.js"></script>`; //можно вставить пользовательский ввод)))) ПОдходит для drop table
@@ -37,9 +37,9 @@ form.addEventListener("submit", (event)=>{
     event.preventDefault();
 });
 
-// input.addEventListener("focus", ()=> {
-//     // console.log("focus");
-// })
+input.addEventListener("focus", ()=> {
+    console.log("focus");
+})
 
 input.addEventListener("blur", ()=> {
     // console.log("blur");
--- Макет/lab3.js
+++ Макет/lab3.js
@@ -1,6 +1,26 @@
 var modal = document.querySelector('.modal-container');
 var closeButton = document.querySelector('.close');
 var modalTriggers = document.querySelectorAll('[data-trigger]');
+var isModalOpen = false;
+var pageYOffset = 0;
+
+var openModal = function() {
+    modal.classList.add('is-open')
+}
+var closeModal = function() {
+    modal.classList.remove('is-open')
+}
+modalTriggers.forEach(function(item) { 
+    item.addEventListener('click', openModal)
+})
+closeButton.addEventListener('click', closeModal)
+document.addEventListener('scroll', onScroll);
+
+var onScroll = function(e) {
+    if (isModalOpen) {
+        window.scrollTo(0, pageYOffset);
+    }
+}
 
 const body = document.body;
 const form = document.getElementById("form1");
@@ -16,17 +36,34 @@ form.addEventListener("submit", (event)=>{
     event.preventDefault();
 });
 
+input1.addEventListener("focus", ()=> {
+    console.log("focus");
+})
+input2.addEventListener("focus", ()=> {
+    console.log("focus");
+})
+
+btn_show.addEventListener("pointerdown", ()=>{
+    input2.setAttribute("type", "text");
+    // console.dir(input2);
+})
+
+btn_show.addEventListener("pointerleave", ()=>{
+    input2.setAttribute("type", "password");
+    // console.dir(input2);
+})
+
 input1.addEventListener("blur", ()=> {
     if (input1.validity.typeMismatch){
         error.textContent = "Не почта";
-        input1.serCustomValidity("не моя почта");
+        input1.serCustomValidity("Не почта");
     }
     else if (input1.validity.tooShort){
     error.textContent = "Мало";
     }
     else{
         error.textContent = "";
-        input.setCustomValidity("");
+        input1.setCustomValidity("");
     }
     console.log(input1.validity);
 });
@@ -34,7 +71,7 @@ input1.addEventListener("blur", ()=> {
 input2.addEventListener("blur", ()=> {
     if (input2.validity.typeMismatch){
         error.textContent = "Неверный пароль";
-        input2.serCustomValidity("не мой пароль");
+        input2.serCustomValidity("Неправильный пароль");
     }
     else if (input2.validity.tooShort){
     error.textContent = "Мало";
@@ -45,32 +82,3 @@ input2.addEventListener("blur", ()=> {
     }
     console.log(input2.validity);
 });
-
-var openModal = function() {
-    modal.classList.add('is-open')
-}
-var closeModal = function() {
-    modal.classList.remove('is-open')
-}
-modalTriggers.forEach(function(item) { 
-    item.addEventListener('click', openModal)
-})
-closeButton.addEventListener('click', closeModal)
-
-var isModalOpen = false;
-var pageYOffset = 0;
-var openModal = function() {
-
-isModalOpen = true;
-scrollY = window.pageYOffset;
-}
-var closeModal = function() {
-isModalOpen = false;
-}
-var onScroll = function(e) {
-    if (isModalOpen) {
-        window.scrollTo(0, scrollY);
-    }
-}
-
-document.addEventListener('scroll', onScroll);
