cfef924c183beb59f8579fd3a5b95ae60303792e
WEB-exam <artsolohome@gmail.com>
2023-03-23 23:45:33 +0300

    nodemon added

--- node-server/app.js
+++ node-server/app.js
@@ -2,16 +2,19 @@ const http = require('node:http')
 
 const host = "127.0.0.1"
 const port = 5500
-
+let i = 0
 const server = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/plain");
-    res.end("hello world");
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`hello, user #${i}`);
 });
 
 server.on("connection", () => {
+    i++;
     console.log("new connect");
 });
 
 server.listen(port, host, () => {
     console.log("bububu");
+    console.log(`Server running at http://${host}:${port}/`);
 })
