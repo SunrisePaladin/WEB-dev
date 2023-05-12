b54b6f6badc28624a30bea2bf76d9dcff7faba65
SunrisePaladin <Revaloise@yandex.ru>
2023-05-12 13:27:49 +0300

    added FindAll method

--- express/services/index.js
+++ express/services/index.js
@@ -1,24 +1,67 @@
+const MongoClient = require('mongodb').MongoClient;
 const db = require('mongodb').Db;
 const {ObjectId} = require('mongodb'); 
-const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient('mongodb://127.0.0.1:27017', { monitorCommands: true });
-const {selectDB : func_search_DB} = require('../configs');
+const {selectDB : func_search_DB} = require('../configs/index');
 
-let mydb = "local";
-let coll = "comments";
+let result = "undefined";
+let mydb = "local"; //локальная бд "local"
+let coll = "comments"; //коллекция "comments"
 
-async function serviceComm(search_name){
+async function serviceSearch(value, mode){
+    console.log("service search is active");
     let connection = await client.connect(function(err){
         if (err){
             throw err;
         }
     });
-    
-    let result = await connection.db(mydb).collection(coll).findOne({name: search_name});
-
+    result = "unregistered mode";
+    switch (mode){
+        case "name":   
+            result = await connection.db(mydb).collection(coll).findOne({name: value});
+            break;
+        case "id":
+            result = await connection.db(mydb).collection(coll).findOne({_id: new ObjectId(value)});
+            break;
+        case "text":
+            result = await connection.db(mydb).collection(coll).findOne({text: value});
+            break;
+        case "time":
+            result = await connection.db(mydb).collection(coll).findOne({cr_date: value});
+            break;    
+        default:
+            result = "unregistered mode";
+            break;
+    }
     return result;
 }
 
+async function serviceAppend(object){
+    console.log("service add is active");
+    let connection = await client.connect(function(err){
+        if (err){
+            throw err;
+        }
+    });
+    result = "Can't add object to DB";
+    result = await connection.db(mydb).collection(coll).insertOne(object);
+    return result;
+}
+
+async function serviceFindAll(){
+    let massResult;
+    console.log("service find all is active");
+    let connection = await client.connect(function(err){
+        if (err){
+            throw err;
+        }
+    });
+    massResult = await connection.db(mydb).collection(coll).find({}).toArray();
+    return massResult;
+}
+
 module.exports = {
-    serviceComm
+    serviceSearch,
+    serviceAppend, 
+    serviceFindAll
 }
