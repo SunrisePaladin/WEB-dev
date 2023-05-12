1f5197be16819b16e5b508bd49c6c88a04f7a6c0
SunrisePaladin <Revaloise@yandex.ru>
2023-05-12 13:27:13 +0300

    enhanced routing

--- express/mongoDB.js
+++ express/mongoDB.js
@@ -1,19 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const { control1: func_control1, control2: func_control2 } = require('./controllers/index');
+//const bodyParser = require('body-parser');
+//const show_router = express.Router();
 const router = express.Router();
+const { control1: func_control1, control2: func_control2, control3: func_control3 } = require('./controllers/index');
 
 function response(){
     console.log("database");
 }
 
+// router.get('/search/', function(req, res){
+//     //undefined
+//     res.status(200).send('You have 3 options to choose from: id, name, text');
+// });
+router.use(express.json());
+
 router.get('/', (req, res) => {
-    res.send('Hello World');
+    res.status(200).send('Hello World V2');
 });
 
-router.get('/show', func_control1);
-//router.get('/add', func_control2);
+router.get('/search/:mode', func_control1);
 
+router.get('/search_all', func_control3);
+
+router.get('/add', func_control2);
+
+//типа свитч бд добавить сюда?
 // router.post('/add/:reqdb', (req, res)=>{
 //     console.log(req.params.reqdb);
 //     let found_value = func_control2(req, res, req.params.reqdb);
@@ -21,7 +32,7 @@ router.get('/show', func_control1);
 //     }
 // );
 
-router.post('/add/:reqdb', func_control2);
+//router.post('/add/:reqdb', func_control2);
 
 module.exports = {
     router,
