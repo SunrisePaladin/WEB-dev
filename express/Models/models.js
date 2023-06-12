72845fdbc7633042a3e336e14e9eb24a1629c692
SunrisePaladin <Revaloise@yandex.ru>
2023-06-12 17:02:01 +0300

    overall update

--- express/Models/models.js
+++ express/Models/models.js
@@ -10,6 +10,8 @@ const {ctrlUserGet: userget,
     safe_insertOne: func_safeInsert,
     authorize: login} = require('./controllers/control');
 
+const {errLog: errCatch} = require('./Errors/errCatcher');
+
 function response(){
     console.log("жаба");
 }
@@ -20,22 +22,23 @@ router.get('/', (req, res) => {
     res.status(200).send('Hello World V3!');
 });
 
-router.get('/user/:id', userget);
-router.post('/user', userpost);
-router.delete('/deleteByKey', deleteDoc);
+router.get('/user/:id', userget, errCatch);
+router.post('/user', userpost, errCatch);
+router.delete('/deleteByKey', deleteDoc, errCatch);
 
 //create
-router.post('/models', login, func_safeInsert); 
+router.post('/models', login, func_safeInsert, errCatch); 
 
 //read
-router.get('/models', func_modelAll);
-router.get('/models/:id', func_model);
+router.get('/models', func_modelAll, errCatch);
+router.get('/models/:id', func_model, errCatch);
 
 //update
-router.put('/models/:id', func_update);
+router.put('/models/:id', func_update, errCatch);
 
 //delete
-router.delete('/models/:id', login, func_delete);
+router.delete('/models/:id', login, func_delete, errCatch);
+
 
 module.exports = {
     router,
--- express/app.js
+++ express/app.js
@@ -1,19 +1,61 @@
 const http = require('node:http');
 const express = require('express');
 const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+// const swaggerOptions = {
+//   definition:{
+//     openapi: '3.0.2',
+//   info: {
+//     title: 'SWAGGER UI yaml',
+//     version: '1.1'
+//   },
+//   servers: [
+//     {
+//       url: '/v3'
+//     }
+//   ],
+//   tags: [
+//       {
+//         name: "user",
+//         description: "Tampering with user credentials"
+//       },
+//       {
+//         name: "models",
+//         description: "Operations on models"
+//       }
+//     ]
+//   },
+//   apis: [
+//     './openapi.yml'
+//   ]
+// }
+
+// const swaggerDocs = swaggerJSDoc(swaggerOptions);
+const openapi_json = require('./openapi.json');
+
 const { router: exportRouterV1 } = require('./express');
 const { router: exportRouterV2 } = require('./mongoDB');
 const { router: exportRouterV3 } = require('./Models/models');
+const {errLog: errCatch} = require('./Models/Errors/errCatcher');
 
 const host = "127.0.0.1";
 const port = 5501;
 const app = express();
 
+let corsOptions = {
+    origin: 'http://127.0.0.1:5501',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.listen(port, () => {
     console.log(`The app is listening on port ${port} and host ${host}`);
 })
 
-app.use(cors());
+//app.use(cors(corsOptions));
+app.use(errCatch);
+//app.use('/swagger-ui', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+app.use('/swagger-ui', swaggerUI.serve, swaggerUI.setup(openapi_json));
 
 app.get('/homepage', (req, res) => {
     res.statusCode = 200;
@@ -24,7 +66,7 @@ app.use('/v1', exportRouterV1);
 app.use('/v2', exportRouterV2);
 app.use('/v3', exportRouterV3);
 
-app.get('/coding', cors(), (req, res) => {     
+app.get('/coding', cors(corsOptions), (req, res) => {     
         res.json({         
         message: 'Happy Coding'    
     }); 
@@ -34,6 +76,7 @@ app.all('/', (req, res) => {
     res.statusCode = 200;
     res.redirect('/homepage');
 });
+
 
 app.use((req, res) => {
     res.status(404);
