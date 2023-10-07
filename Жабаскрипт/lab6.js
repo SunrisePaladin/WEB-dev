73a10547b8154eb1c45ca51d7118394722299d93
SunrisePaladin <Revaloise@yandex.ru>
2023-10-07 20:19:41 +0300

    added last work

--- Жабаскрипт/lab6.js
+++ Жабаскрипт/lab6.js
@@ -1,3 +1,4 @@
+
 let v = new Vue({
     el: ".vue",
     data:{
@@ -16,3 +17,39 @@ let s = new Vue({
         prog: ""
     }
 })
+
+let blog = new Vue({
+    el: ".vue-profile",
+    data:{
+        name: "ArtSolo",
+        biography: "Преподаватель школы программирования Пиксель"
+    },
+    methods: {
+        name_change: function(){
+            this.name = prompt("новое имя");
+        },
+        bio_change: function(){
+            this.biography = prompt("новое описание");
+        }
+    }
+})
+
+let time = new Date(2023, 10, 7, 19, 39, 30, 100);
+let bloghistory = "\r\n";
+
+let blog_wall = new Vue({
+    el: ".vue-post",
+    data:{
+        blog: bloghistory
+    },
+    methods: {
+        add_post: function(){
+            bloghistory = (`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()} – ${blog.name}(${blog.biography}) сказал(-а): ${prompt("новое сообщение")}`);
+            this.blog = bloghistory;
+            document.getElementById("wall").innerHTML += bloghistory + "<br>";
+        }
+    }
+})
+
+// import date from 'date-and-time';
+// const date = require('date-and-time');
